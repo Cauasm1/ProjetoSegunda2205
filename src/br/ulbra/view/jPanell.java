10444c620177fcbdf310b97e94d107081a239475
cauastorck <cauam2498@gmail.com>
2023-05-22 21:43:48 -0300

    Programa exemplo da aula segunda-feira 22/05 - primeira versão Desenvolvido por Cauã

---
+++ src/br/ulbra/view/jPanell.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.view;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author s.lucas
+ */
+class jPanell {
+
+    static void setBackground(Color RED) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
